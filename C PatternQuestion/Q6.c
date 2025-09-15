6b6a908fbe49bb90aef7c08e96093eba3a41e4bb
prashant.parmar.cg@gmail.com <prashant.parmar.cg@gmail.com>
2025-09-15 10:10:34 +0530

    C Pattern Question

---
+++ C PatternQuestion/Q6.c
@@ -0,0 +1,8 @@
+ int n;
+    scanf("%d",&n);
+    for(int i=65; i<65+n; i++){
+        for(int j=65; j<65+n; j++){
+        printf("%c ",j);
+        }
+        printf("\n");
+    }
---
+++ C PatternQuestion/Q9.c
@@ -0,0 +1,7 @@
+ int n;
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<=i; j++){
+        printf("%c",65+j);
+        }
+        printf("\n");
